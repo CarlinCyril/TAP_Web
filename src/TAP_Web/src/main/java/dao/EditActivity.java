1dc2211b6853ff90af4b7bbf54c942822d748b9d
demarquq <38251935+demarquq@users.noreply.github.com>
2018-04-26 14:15:07 +0200

    Add files via upload

--- src/TAP_Web/src/main/java/dao/EditActivity.java
+++ src/TAP_Web/src/main/java/dao/EditActivity.java
@@ -35,10 +35,10 @@ public class EditActivity extends AbstractDataBaseDAO{
         ResultSet resultSet = null;
         try {
         	Connection co = dataSource.getConnection();
-        	statement = co.prepareStatement("SELECT MAX(ID_Activity) FROM Activity");
+        	statement = co.prepareStatement("SELECT MAX(to_number(ID_Activity)) FROM Activity");
         	resultSet = statement.executeQuery();
         	while(resultSet.next()) {
-        		id = Integer.parseInt(resultSet.getString("ID_Activity")) + 1;
+        		id = Integer.parseInt(resultSet.getString("ID_Activivty")) + 1;
         	}
         	}catch(SQLException se){
         		System.out.println(se.getMessage());
@@ -59,6 +59,42 @@ public class EditActivity extends AbstractDataBaseDAO{
             try { connection.close(); } catch(Exception e){ /* ignored */}
     	}
     }
+    
+    public void addGroup(Group grpAct) {
+        Integer id = 0;
+    	Connection connection = null;
+        PreparedStatement statement = null;
+        ResultSet resultSet = null;
+        try {
+        	Connection co = dataSource.getConnection();
+        	statement = co.prepareStatement("SELECT MAX(to_number(ID_Group)) FROM GroupActivity");
+        	resultSet = statement.executeQuery();
+        	while(resultSet.next()) {
+        		id = Integer.parseInt(resultSet.getString("ID_Group")) + 1;
+        	}
+        	}catch(SQLException se){
+        		System.out.println(se.getMessage());
+        }
+        try {
+            connection = dataSource.getConnection();
+            statement = connection.prepareStatement(
+                    "INSERT INTO GroupActivity VALUEs(?, ?, ?, ?, ?, ?)");
+            statement.setString(1, id.toString());
+            statement.setString(2, grpAct.getActivity());
+            statement.setString(3, grpAct.getDayOfTheWeek());
+            statement.setString(4, grpAct.getStartTime());
+            statement.setString(5, grpAct.getEndTime());
+            statement.setString(6, grpAct.getEnrollment().toString());
+            resultSet = statement.executeQuery();
+    } catch(SQLException se){
+            System.out.println(se.getMessage());
+    	}finally {
+            try { resultSet.close(); } catch(Exception e){ /* ignored */}
+            try { statement.close(); } catch(Exception e){ /* ignored */}
+            try { connection.close(); } catch(Exception e){ /* ignored */}
+    	}
+    }
+    
     public void delAct(String id) {
     	Connection connection = null;
         PreparedStatement statement = null;
@@ -71,7 +107,22 @@ public class EditActivity extends AbstractDataBaseDAO{
             resultSet = statement.executeQuery();
 	    } catch(SQLException se){
             System.out.println(se.getMessage());
+	    }}
+	    
+	    public void delGroup(String id) {
+	    	Connection connection = null;
+	        PreparedStatement statement = null;
+	        ResultSet resultSet = null;
+		    try {
+		    	connection = dataSource.getConnection();
+	            statement = connection.prepareStatement(
+	                    "DELETE FROM GroupActivity WHERE 'ID_Group' = ?");
+	            statement.setString(1, id);
+	            resultSet = statement.executeQuery();
+		    } catch(SQLException se){
+	            System.out.println(se.getMessage());
+		    }
 	    }
     }
 
-}
+
